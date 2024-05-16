5d0fc9941018dea467d609ae88434b3d9e779cee
DeathCookieOP <procookieop@gmail.com>
2024-05-16 11:05:10 -0700

    Updated Index Page

--- vite-project/src/pages/Index.jsx
+++ vite-project/src/pages/Index.jsx
@@ -1,25 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import styles from './Index.module.css'
+import React, { useEffect, useState } from 'react';
+import styles from './Index.module.css';
 
 function Index() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     fetch('http://localhost:5000/games_json')
       .then(response => response.json())
       .then(data => setData(data))
-      .catch(err => console.log(err))
-  }, [])
-
-
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div className={styles.index_page_container}>
-      {/* <p>{data[0].game_id}</p> */}
-      {/* {data.length > 0 && data[0].game_id && <h1>Game Information</h1>} */}
-      <h1 className={styles.mainText}>Game Information</h1>
-      <h2 className={styles.indexText}>Look at some information on your favorite games</h2>
+      <div>
+        <h1 className={styles.indexText}>Game Information</h1>
+        <p className={styles.mainText}>
+          Explore the fascinating world of your favorite games.
+        </p>
+        <a href="/login"><button className={styles.getStartedButton}>Get Started</button></a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Index
+export default Index;
